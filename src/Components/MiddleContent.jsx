3e99b8e8ea044e3642f84c584719d6cbf883bce0
chrissiku <chrissiku5@gmail.com>
2022-09-20 20:17:24 +0200

    Implement and style the create post part

--- src/Components/MiddleContent.jsx
+++ src/Components/MiddleContent.jsx
@@ -1,7 +1,26 @@
+import avatar from "../images/avatar.jpg";
+import photo from "../images/photo.png";
+import video from "../images/video.png";
+import event from "../images/event.png";
 const MiddleContent = () => {
   return (
     <>
-      <div className="middle-content">Middle content</div>
+      <div className="middle-content">
+        <div className="create-post">
+          <div className="create-post-input">
+            <img src={avatar} alt="avatar" />
+            <textarea rows={2} placeholder="Start a post"></textarea>
+          </div>
+          <div className="create-post-links">
+            <ul>
+              <li><img src={photo} alt="add pic" />Photo</li>
+              <li><img src={video} alt="add video" />Video</li>
+              <li><img src={event} alt="add event " />Event</li>
+              <li>Post</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
