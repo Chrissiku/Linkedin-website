c90487393cb02bedc0b205f23ae75d72b3183784
chrissiku <chrissiku5@gmail.com>
2022-09-20 15:50:45 +0200

    Implement and style the sidebar-news

--- src/Components/RightSidebar.jsx
+++ src/Components/RightSidebar.jsx
@@ -1,7 +1,30 @@
+import { Link } from "react-router-dom";
+import more from "../images/more.png";
+
 const RightSidebar = () => {
   return (
     <>
-      <div className="right-sidebar">Right sidebar</div>
+      <div className="right-sidebar">
+        <div className="sidebar-news">
+          <img src={more} alt="more info" className="info-icon" />
+          <h3>Trending News</h3>
+          <Link to="/">High demand for skilled developers</Link>
+          <span>1d ago &middot; 10,999 readers</span>
+
+          <Link to="/">High demand for skilled developers</Link>
+          <span>1d ago &middot; 10,999 readers</span>
+
+          <Link to="/">High demand for skilled developers</Link>
+          <span>1d ago &middot; 10,999 readers</span>
+
+          <Link to="/">High demand for skilled developers</Link>
+          <span>1d ago &middot; 10,999 readers</span>
+
+          <Link to="/" className="read-more">
+            Read more
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
